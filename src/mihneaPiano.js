ff9fa0cbce3ca421e506b331d5919b0239d0eb4b
mihneavlad <mihnel@yahoo.com>
2018-12-02 10:45:16 +0100

    Created Piano component

---
+++ src/mihneaPiano.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Soundfont from 'soundfont-player';
+
+class mihneaPiano extends React.Component {
+
+}
+
+
+export default mihneaPiano;
