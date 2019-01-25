aeffda2599b3eef303f7cc7e1d16c48bcfe1f028
mihneavlad <mihnel@yahoo.com>
2019-01-25 08:52:30 +0100

    Updates

---
+++ precache-manifest.e74088c2f7b034e319895d9017da1411.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b34048b1cff9578fbf0",
+    "url": "/react-mihnea-piano/static/js/runtime~main.6b34048b.js"
+  },
+  {
+    "revision": "9477516bd898160fc513",
+    "url": "/react-mihnea-piano/static/js/main.9477516b.chunk.js"
+  },
+  {
+    "revision": "574ec49190ea8eab4f29",
+    "url": "/react-mihnea-piano/static/js/1.574ec491.chunk.js"
+  },
+  {
+    "revision": "9477516bd898160fc513",
+    "url": "/react-mihnea-piano/static/css/main.59834173.chunk.css"
+  },
+  {
+    "revision": "574ec49190ea8eab4f29",
+    "url": "/react-mihnea-piano/static/css/1.ba297653.chunk.css"
+  },
+  {
+    "revision": "9ca05ce5d607538d576ff4ab14ac67e6",
+    "url": "/react-mihnea-piano/index.html"
+  }
+];
